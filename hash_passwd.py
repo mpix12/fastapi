f1c4868091bc8106fe610fb68cf0ab8b5fddab5d
mpix12 <pande.mandy@gmail.com>
2023-01-16 11:35:26 +0800

    Updating hashing functionality.

---
+++ hash_passwd.py
@@ -0,0 +1,25 @@
+from passlib.context import CryptContext
+
+"""
+Utility to decrypt and verify plain text password
+"""
+passwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def bcrypt_passwd(passwd):
+    """
+    Encrypt password
+    :param passwd: plain text password
+    :return: Hashed password
+    """
+    return passwd_context.hash(passwd)
+
+
+def verify(hashed_passwd: str, plain_passwd: str):
+    """
+    Verify plain text and hashed password
+    :param hashed_passwd: Hashed password
+    :param plain_passwd: plain test password
+    :return: Boolean values: True or False
+    """
+    return passwd_context.verify(plain_passwd, hashed_passwd)
